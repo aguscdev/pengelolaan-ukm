159fddae3e50a788a389e56919ad4c896a48699e
agusc <agusc.dev02@gmail.com>
2019-11-05 14:46:37 +0700

    5-11-2019 2:46

--- config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$currency = 'Rp.';
-$koneksi = mysqli_connect("localhost","root","","umkm");
-
-// check koneksi
-if (mysqli_connect_errno()) {
-	echo "koneksi database gagal :" .mysqli_connect_error();
-}
-?>
