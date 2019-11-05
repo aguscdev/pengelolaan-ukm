ffcb2a81041f89cedb819e950333477da0b774cf
agusc <agusc.dev02@gmail.com>
2019-11-05 16:24:51 +0700

    5-11-2019 4:24

--- kontak.php
+++ kontak.php
@@ -29,7 +29,7 @@ $query2 = mysqli_query($koneksi,"SELECT * FROM produk ORDER BY kategori_id ASC")
 		<div class="w3_agileits_contact_grids">
 			<div class="col-md-6 w3_agileits_contact_grid_left">
 				<div class="agile_map">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.3905851087434!2d-34.90500565012194!3d-8.061582082752993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab18d90992e4ab%3A0x8e83c4afabe39a3a!2sSport+Club+Do+Recife!5e0!3m2!1sen!2sin!4v1478684415917" style="border:0"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.312769801949!2d106.55810771446575!3d-6.22242586267421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fe74eed2a5d1%3A0x10c2f57db3bc5d23!2sSTIE+%26+STMIK+Insan+Pembangunan!5e0!3m2!1sid!2sid!4v1545060110576" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 				<div class="agileits_w3layouts_map_pos">
 					<div class="agileits_w3layouts_map_pos1">
